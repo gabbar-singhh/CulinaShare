107507c1809838f57669ff8bc5ded777d7c21b4f
gabbar-singhh <himanshup1308@gmail.com>
2024-02-19 17:16:16 +0530

    mealdb api implemented

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import styles from './Discover.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./Discover.module.css";
+import axios from "axios";
 
 const Discover = () => {
-  return (
-    <section className={styles.discover_main}>Discover</section>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default Discover
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then(function (response) {
+        // handle success
+        console.log("response", response.data.categories);
+        setData(response.data.categories);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
+
+  return (
+    <section className={styles.discover_main}>
+      <h1>Discover Recipes</h1>
+
+      <ul>
+        {data.map((item) => {
+          return <li>{item.strCategory}</li>;
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Discover;
