ec3f6cb3cfd15367d4027393c7c937f5eb87aefc
Himanshu <110885026+gabbar-singhh@users.noreply.github.com>
2024-02-29 11:09:51 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
           content="CulinaShare - Where Every Recipe Tells a Story!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <main className={styles.main}>
         <NavigationBar />
